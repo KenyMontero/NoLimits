1d27ccbdc777b0614eb68027772dfd9f06730d6f
IsaacGutierrez <Inggutierrez1113@hotmail.com>
2020-11-23 14:35:36 -0400

    Se agrego visualiacion 3d sobre disenios de casas familiares

--- NoLimits/App_Start/RouteConfig.cs
+++ NoLimits/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NoLimits
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "NoLimits", action = "NoLimit", id = UrlParameter.Optional }
             );
         }
     }
